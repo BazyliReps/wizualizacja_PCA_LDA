da83bb9662c3e6761f056b724cad0e4700fdec76
bazyli <b.reps@optimali.io>
2020-03-07 23:39:48 +0100

    dodany opis w latexie, poprawiony skrypt analizy

--- proj/analiza_PCA.R
+++ proj/analiza_PCA.R
@@ -6,22 +6,28 @@ heart <- read.csv("heart.csv")
 
 heart.pr <- prcomp(heart[c(2:length(colnames(heart)))], center = TRUE, scale = TRUE)
 
-
+# wyznaczenie wariancji zawartej w dwóch największych składnikach wiodących
 pc1_var <- as.double(summary(heart.pr)$importance[,1][2])
 pc2_var <- as.double(summary(heart.pr)$importance[,2][2])
 print(sprintf('Wariancja danych zawarta w pierwszym składniku wiodącym: %s%%', format(round(pc1_var, 2), nsmall = 2)))
 print(sprintf('Wariancja danych zawarta w drugim składniku wiodącym: %s%%', format(round(pc2_var, 2), nsmall = 2)))
 
-screeplot(heart.pr, type = "l", npcs = 15, main = "Screeplot of the first 10 PCs")
+
+# Wykres wartości własnych dla 15 największych składników wiodących
+pdf('docs/wartosci_wlasne.pdf')
+screeplot(heart.pr, type = "l", npcs = 15, main = "Wykres wartości własnych dla 15 największych składników wiodących")
 abline(h = 1, col="red", lty=5)
-legend("topright", legend=c("Eigenvalue = 1"),
+legend("topright", legend=c("Wartość własna = 1"),
        col=c("red"), lty=5, cex=0.6)
+dev.off()
 
-
+# kumulacyjny wykres wariancji
 cumpro <- cumsum(heart.pr$sdev^2 / sum(heart.pr$sdev^2))
-plot(cumpro[0:45], xlab = "PC #", ylab = "Amount of explained variance", main = "Cumulative variance plot")
+pdf('docs/wariancja_kum.pdf')
+plot(cumpro[0:45], xlab = "Składnik wiodący", ylab = "Wariancja wyjaśniona", main = "Kumulatywny wykres wariancji")
 legend("topleft", legend=c("Cut-off @ PC6"),
        col=c("blue"), lty=5, cex=0.6)
+dev.off()
 
 x_label <- sprintf('składnik wiodący 1: (%s%%)', format(round(pc1_var, 2), nsmall = 2))
 y_label <- sprintf('składnik wiodący 2: (%s%%)', format(round(pc2_var, 2), nsmall = 2))
@@ -31,6 +37,7 @@ plot(heart.pr$x[,1],heart.pr$x[,2], xlab=x_label, ylab = y_label, main = plot_ti
 
 # to wrzuććie do konsoli, ze skryptu mi się nie rysuje a ładne jest
 library("factoextra")
+pdf('docs/klasy.pdf')
 fviz_pca_ind(heart.pr, geom.ind = "point", pointshape = 21, 
              pointsize = 2, 
              fill.ind = heart$OVERALL_DIAGNOSIS, 
@@ -43,3 +50,4 @@ fviz_pca_ind(heart.pr, geom.ind = "point", pointshape = 21,
              legend.title = "Diagnoza") +
   ggtitle("Dane z zaznaczonymi klasami") +
   theme(plot.title = element_text(hjust = 0.5))
+dev.off()
